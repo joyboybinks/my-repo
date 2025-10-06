ba0fd108c2e83419e9724588e4a4716b40ce27bc
joyboybinks <boska761z@gmail.com>
2025-10-06 19:16:18 +0200

    my second commit

--- main.go
+++ main.go
@@ -1,15 +1,8 @@
 package main 
 
-import "fmt"
-
 func PrintIfNot(str string) string {
     if len(str) < 3 {
         return "G\n"
     }
     return "invalid Input\n"
-}
-
-func main() {
-    fmt.Print(PrintIfNot("hi"))   // chaîne de 2 caractères → "G\n"
-    fmt.Print(PrintIfNot("hello")) // chaîne de 5 caractères → "invalid Input\n"
 }
